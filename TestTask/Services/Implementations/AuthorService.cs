262fe8f361604cf0090e297c79239702e2096127
Igor Evtushenko <evtushwork@gmail.com>
2025-01-19 20:52:35 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    internal class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public AuthorService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            var maxLength = await dbContext.Books
+                .AsNoTracking()
+                .MaxAsync(b => b.Title.Length);
+
+            return dbContext.Books
+                .AsNoTracking()
+                .Include(b => b.Author)
+                .Where(b => b.Title.Length == maxLength)
+                .Select(b => b.Author)
+                .AsEnumerable()
+                .MinBy(a => a.Id) 
+                ?? throw new Exception("There are no books");
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            return await dbContext.Books
+                .AsNoTracking()
+                .Where(b => b.PublishDate >= new DateTime(2015, 1, 1))
+                .GroupBy(b => b.Author)
+                .Where(i => (i.Count() & 1) == 0)
+                .Select(g => g.Key)
+                .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    internal class BookService : IBookService
+    {
+        private readonly DateTime CarolusRexPublishingDate = new DateTime(2012, 5, 25);
+        private readonly ApplicationDbContext dbContext;
+
+        public BookService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            return dbContext.Books
+                .AsNoTracking()
+                .AsEnumerable()
+                .MaxBy(b => b.Price) ?? throw new Exception("Book is missing");
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await dbContext.Books
+                .AsNoTracking()
+                .Where(b => b.Title.Contains("Red") && b.PublishDate > CarolusRexPublishingDate)
+                .ToListAsync();
+        }
+    }
+}
